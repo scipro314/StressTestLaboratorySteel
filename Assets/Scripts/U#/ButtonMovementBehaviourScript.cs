7ce235f424b31a9abcae9369f89d1d0dadf03fa3
Jorge Antonio Escobar <105883796+scipro314@users.noreply.github.com>
2025-01-30 10:40:06 -0600

    ButtonTestTensileMachine

---
+++ Assets/Scripts/U#/ButtonMovementBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ButtonMovementBehaviourScript : UdonSharpBehaviour
+{ 
+    public float upperRestriction = 0.87f;
+    public float lowerRestriction = 0.58f;
+    private Vector3 initialPositionButton;
+
+    void Start()
+    {
+        initialPositionButton = transform.position;
+    }
+
+    void Update()
+    {
+        float yPosition = transform.position.y;
+        if (yPosition > upperRestriction)
+        {
+            yPosition = upperRestriction;
+        }
+        else if (yPosition < lowerRestriction) 
+        {
+            yPosition = lowerRestriction;
+        }
+        transform.position = new Vector3(initialPositionButton.x, yPosition, initialPositionButton.z);
+    }
+
+}
